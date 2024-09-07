c2b60a4becdc9151e45bc76f7f90f4e0b2eba5d1
sherifshouaib <sherifshouaib22@gmail.com>
2024-09-07 14:52:51 +0300

    add featured listview to home

--- lib/Features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/Features/home/presentation/views/widgets/custom_app_bar.dart
@@ -8,16 +8,16 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24,vertical: 40),
+      padding: const EdgeInsets.only(left: 24, right: 24, top: 40, bottom: 20),
       child: Row(
         children: [
-          Image.asset(AssetsData.logo,height: 18,),
+          Image.asset(
+            AssetsData.logo,
+            height: 18,
+          ),
           const Spacer(),
           IconButton(
-            onPressed: () {
-      
-            },
-            
+            onPressed: () {},
             icon: const Icon(
               FontAwesomeIcons.magnifyingGlass,
               size: 24,
--- lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -1,23 +1,20 @@
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          decoration:  BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            color: Colors.red,
-            image:const  DecorationImage(
-              fit: BoxFit.fill,
-              image: AssetImage(AssetsData.testImage),
-            ),
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        decoration:  BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.red,
+          image:const  DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(AssetsData.testImage),
           ),
         ),
       ),
